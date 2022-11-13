cec12e1e3bd8cab7b102453166b15d9b08f36f2b
Luis Molina <molicode1347@gmail.com>
2022-11-13 13:44:42 -0300

    Spring Bean Cope Example

--- src/main/java/molicode/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/molicode/springframework/sfgdi/SfgDiApplication.java
@@ -6,6 +6,8 @@ import molicode.springframework.sfgdi.controller.MyController;
 import molicode.springframework.sfgdi.controller.PetController;
 import molicode.springframework.sfgdi.controller.PropertyInjectedController;
 import molicode.springframework.sfgdi.controller.SetterInjectedController;
+import molicode.springframework.sfgdi.services.PrototypeBean;
+import molicode.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -42,6 +44,18 @@ public class SfgDiApplication {
     ConstructorInjectedController constructorInjectedController =
         (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
     System.out.println(constructorInjectedController.getGreeting());
+
+    System.out.println("--- Bean Scopes ----------");
+    SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+    System.out.println(singletonBean1.getMyScope());
+    SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+    System.out.println(singletonBean2.getMyScope());
+
+    PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+    System.out.println(prototypeBean1.getMyScope());
+    PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+    System.out.println(prototypeBean2.getMyScope());
+
   }
 
 }
