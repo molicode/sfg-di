c8610b121bab1453268db23f9d83ec1ff53b4138
Luis Molina <molicode1347@gmail.com>
2022-11-15 16:34:18 -0300

    Spring Boot Application.properties

--- src/main/java/molicode/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/molicode/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -14,9 +14,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
 
